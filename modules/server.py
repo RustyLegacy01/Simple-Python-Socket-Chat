884916da7364d56dcfe6036d2406797f6214e726
nobledev <80450196+RustyLegacy01@users.noreply.github.com>
2025-09-16 09:36:10 +0200

    Add display of local IP on serverside.
    Allows for clients to be made aware of which local IP to connect to.

--- modules/server.py
+++ modules/server.py
@@ -9,11 +9,23 @@ class Server:
         self.__serverIP: str = serverIP
         self.__port: int = port
 
+    def getLocalIp(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(("8.8.8.8", 80))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = "127.0.0.1"
+        finally:
+            s.close()
+        return ip
+
     def startServer(self):
         self.__server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Open TCP Connection
         self.__server.bind((self.__serverIP, self.__port))
         self.__server.listen(0)
         print(f"Listening on {self.__serverIP}:{self.__port}")
+        print(f"Your local IP: {self.getLocalIp()}")
         clientSocket, clientAddress = self.__server.accept()
         print(f"Accepted connection from {clientAddress[0]}:{clientAddress[1]}")
 
